5437c4de5f1dcb5c5857e16a7519b80100f4ac1b
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2024-10-18 18:50:07 +0530

    Project Completed

---
+++ Sem 1 project/Online-Food-Ordering-System/include/footer.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+    <div class="container">
+
+
+        <div class="bottom-footer">
+            <div class="row">
+                <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                    <h5>Payment Options</h5>
+                    <ul>
+                        <li>
+                            <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 address color-gray">
+                    <h5>Address</h5>
+                    <p>Shri V. J. Modha College, Porbandar.</p>
+                    <h5>Phone: +91-96387 98285</a></h5>
+                </div>
+                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                    <h5>Message from Chirag Thanki</h5>
+                    <p>Thank you so much for visiting. Have a great day.</p>
+                    <p>Design and created by <a href="https://chigs-thanky.github.io/Chirag-J-Thanki-Portfolio/"
+                            target="_blank"><b>Chirag Thanki</b></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</footer>
