8caccdf42cddb68280ebfb630bc0206364dec2cf
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-09-30 12:00:04 +0530

    Projects

---
+++ Sem 1 project/Online-Food-Ordering-System/admin/include/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer" style="text-align: center;">
+    Design and created by <a href="https://chigs-thanky.github.io/Chirag-J-Thanki-Portfolio/" target="_blank"><b>Chirag
+            Thanki</b></a>
+</footer>
